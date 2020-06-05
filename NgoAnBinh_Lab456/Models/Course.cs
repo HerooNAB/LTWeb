f5c5e1e0c267002374815316e49ea64079d55de8
Administrator <Administrator@PC225>
2020-06-05 16:22:16 +0700

    05/06/2020 - 4h22

--- NgoAnBinh_Lab456/Models/Course.cs
+++ NgoAnBinh_Lab456/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,17 @@ namespace NgoAnBinh_Lab456.Models
 {
     public class Course
     {
-       
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime datetime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+
     }
 }
---
+++ NgoAnBinh_Lab456/ViewModels/CourseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NgoAnBinh_Lab456.ViewModels
+{
+    public class CourseModel
+    {
+        public string Place { get; set; }
+        public string Time { get; set; }
+        public string Date { get; set; }
+    }
+}
